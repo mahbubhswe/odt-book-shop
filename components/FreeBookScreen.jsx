9d58075cdef2a7c3f4064679795bb96924971804
mahbubhswe <mahbubhswe@gmail.com>
2022-10-05 17:10:35 +0600

    add hover effect

--- components/FreeBookScreen.jsx
+++ components/FreeBookScreen.jsx
@@ -1,5 +1,6 @@
 import { Grid, Pagination, Stack, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 export default function FreeBookScreen({ freeBookList }) {
   return (
@@ -14,14 +15,26 @@ export default function FreeBookScreen({ freeBookList }) {
           <Grid
             item
             key={item.id}
-            sx={{ widht: "80px", height: "100px", m: "30px" }}
+            sx={{
+              widht: "80px",
+              height: "100px",
+              m: "30px",
+              "&:hover": {
+                boxShadow:
+                  "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+              },
+            }}
           >
-            <Image src={item.img} height={100} width={80} quality={100} />
+            <Link href="/" passHref>
+              <a>
+                <Image src={item.img} height={100} width={80} quality={100} />
+              </a>
+            </Link>
           </Grid>
         ))}
       </Grid>
 
-      <Stack sx={{py:"20px" }} justifyContent="center" alignItems="center">
+      <Stack sx={{ py: "20px" }} justifyContent="center" alignItems="center">
         <Pagination count={5} sx={{ fontStyle: "italic" }} />
       </Stack>
     </React.Fragment>
