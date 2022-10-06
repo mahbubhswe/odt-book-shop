c0a775495b14cf846e2b6d1657b88d3c5bcde8a5
mahbubhswe <mahbubhswe@gmail.com>
2022-10-06 23:38:24 +0600

    add sidebar in mobile

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,15 +1,28 @@
-import { Button, Drawer, IconButton, Stack, Typography } from "@mui/material";
+import { Drawer, IconButton, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import styles from "../styles/navBar.module.css";
 import { useState } from "react";
+import SideBar from "../components/SideBar";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 export default function NavBar() {
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <React.Fragment>
       <Stack direction="row" sx={{ mt: "25px" }}>
+        <IconButton
+          onClick={() => setOpen(true)}
+          sx={{
+            display: { xs: "block", sm: "block", md: "none", lg: "none" },
+            ml: "20px",
+          }}
+        >
+          <ChevronRightIcon />
+        </IconButton>
         <Typography
           sx={{
             display: { xs: "block", sm: "block", md: "none", lg: "none" },
@@ -54,7 +67,7 @@ export default function NavBar() {
           <MenuIcon />
         </IconButton>
       </Stack>
-      {/* mobile menu */}
+      {/* mobile menu for navbar*/}
       <Drawer open={show} onClose={() => setShow(false)}>
         <Stack sx={{ padding: "20px", width: "100vw" }} spacing={1}>
           <div style={{ display: "flex", justifyContent: "right" }}>
@@ -76,6 +89,22 @@ export default function NavBar() {
               <a className={styles.authBtn}>Login/Sign up</a>
             </Link>
           </Stack>
+        </Stack>
+      </Drawer>
+      {/* mobile menu for sidebar*/}
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <Stack sx={{  padding: "20px", width: "100vw" }}>
+          <div style={{ display: "flex", justifyContent: "right" }}>
+            <IconButton
+              size="small"
+              onClick={() => setOpen(false)}
+              type="button"
+            >
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+
+          <SideBar />
         </Stack>
       </Drawer>
     </React.Fragment>
