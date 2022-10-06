705e6d38a23ecbc2aab1520c13bb9fa0063747ab
mahbubhswe <mahbubhswe@gmail.com>
2022-10-06 16:30:31 +0600

    add link

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div style={{height:"100vh",display:"grid",placeContent:"center"}}>
       <Link href="/book/free-book-list">
-        <a>Go to book list page</a>
+        <a>free book list</a>
+      </Link>
+      <Link href="/book/free-book-list">
+        <a>paid book list</a>
       </Link>
     </div>
   );
